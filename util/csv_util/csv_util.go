8b55bf50421dd46aef8d18080321ed1a7f537644
王柏鈞 Royce Wang <Royce.Wang@wiadvance.com>
2025-04-15 17:34:39 +0800

    [feat] add CsvToJSON util [chore] add some docker file

---
+++ util/csv_util/csv_util.go
@@ -0,0 +1,40 @@
+package csv_util
+
+import (
+	"bytes"
+	"encoding/csv"
+	"errors"
+)
+
+type CsvUtilError error
+
+var (
+	ErrEmptyCSV CsvUtilError = errors.New("CSV file has no data or only headers")
+)
+
+func CsvToJSON(csvData []byte) ([]map[string]string, error) {
+	reader := csv.NewReader(bytes.NewReader(csvData))
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	if len(records) <= 1 {
+		return nil, ErrEmptyCSV
+	}
+
+	headers := records[0]
+	var result []map[string]string
+
+	for _, record := range records[1:] {
+		row := make(map[string]string)
+		for i, value := range record {
+			if i < len(headers) {
+				row[headers[i]] = value
+			}
+		}
+		result = append(result, row)
+	}
+
+	return result, nil
+}
